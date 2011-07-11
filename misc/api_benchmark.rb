af58fe90910e9dd8e5f3f3212c2a6a8653f3d619
Ben Shapiro <ben@getdown.org>
2011-07-11 12:33:40 -0500

    updated to provide timing

--- misc/api_benchmark.rb
+++ misc/api_benchmark.rb
@@ -2,15 +2,33 @@ require "net/http"
 require "uri"
 require "benchmark"
 
-mpac_uri = URI.parse("http://localhost:3000/worlds/1/megatiles.mpac?auth_token=xxjfRu0xEH74ofeHhPP0")
-json_uri = URI.parse("http://localhost:3000/worlds/1/megatiles.json?auth_token=xxjfRu0xEH74ofeHhPP0")
-xml_uri = URI.parse("http://localhost:3000/worlds/1/megatiles.xml?auth_token=xxjfRu0xEH74ofeHhPP0")
+auth_token = "FOOFOO"
+url = "http://tf.dev.mirerca.com/worlds/2/megatiles.FORMAT?x_min=400&y_min=400&x_max=420&y_max=420&auth_token=#{auth_token}"
 
-puts "MessagePack:"
-Benchmark.measure { 100.times {Net::HTTP.get_response mpac_uri} }
 
 puts "JSON"
-Benchmark.measure { 100.times {Net::HTTP.get_response json_uri} }
+bm = Benchmark.measure do
+  uri = URI.parse(url.sub("FORMAT", "json"))
+  100.times do
+    Net::HTTP.get_response uri
+  end
+end
+puts bm
 
 puts "XML"
-Benchmark.measure { 100.times {Net::HTTP.get_response xml_uri} }
+bm = Benchmark.measure do
+  uri = URI.parse(url.sub("FORMAT", "xml"))
+  100.times do 
+    Net::HTTP.get_response uri
+  end
+end
+puts bm
+
+puts "MessagePack:"
+bm = Benchmark.measure do
+  uri = URI.parse(url.sub("FORMAT", "mpac"))
+  100.times do 
+    Net::HTTP.get_response uri
+  end
+end
+puts bm
