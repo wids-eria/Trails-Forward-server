117c2e0dfe2876401517da0cc6d08d453e24b76a
yangk <yangk@yangk-desktop.(none)>
2011-07-14 16:19:16 -0500

    Updated the bird and tree tickers to use new layout.

--- app/lib/tickers/critter_ticker.rb
+++ app/lib/tickers/critter_ticker.rb
@@ -2,9 +2,9 @@ require 'narray'
 
 class CritterTicker
   
-  attr_reader :deciduous, :coniferous, :mixed, :world
+  attr_reader :land, :world
   
-  SpeciesTickers = [LigerTicker]
+  SpeciesTickers = [ChickadeeTicker, CuckooTicker, FlycatcherTicker, WarblerTicker, WoodthrushTicker]
   
   def initialize(world)
     @world = world
@@ -18,32 +18,25 @@ class CritterTicker
   end  
   
   def get_species_data
-    world_size = @world.width * @world.height
-    
-    deciduousness = 0
-    @deciduous = NArray.byte(@world.width, @world.height)
-    coniferousness = 0
-    @coniferous = NArray.byte(@world.width, @world.height)
-    mixedness = 0
-    @mixed = NArray.byte(@world.width, @world.height)
+  
+    @land = NArray.byte(@world.width, @world.height)
+    @land.fill!(0)
     
     ResourceTile.where(:world_id => @world.id).find_in_batches do |group|
       group.each do |rt|
         x = rt.x
         y = rt.y
         case rt.tree_species
-          when ResourceTile::Verbiage[:tree_species][:coniferous]
-            coniferousness += 1
-            @coniferous[x,y] = 1
           when ResourceTile::Verbiage[:tree_species][:deciduous]
-            deciduousness += 1
-            @deciduous[x,y] = 1
+            @land[x,y] = 1
+          when ResourceTile::Verbiage[:tree_species][:coniferous]
+            @land[x,y] = 2
           when ResourceTile::Verbiage[:tree_species][:mixed]
-            mixedness += 1
-            @mixed[x,y] = 1
+            @land[x,y] = 3
         end #case
       end #group
-    end #find_in_batches    
+    end #find_in_batches
+    
   end
     
 end
--- app/lib/tickers/tree_ticker.rb
+++ app/lib/tickers/tree_ticker.rb
@@ -1,10 +1,66 @@
+require 'narray'
+require 'matrix_utils'
+require 'ticker_utils'
+
 class TreeTicker
   def self.tick(world)
+  
+    # get tree data for world 
+    @old_tree_size = NArray.float(world.width, world.height)
+    @decid = NArray.float(world.width, world.height)
+    @conifer = NArray.float(world.width, world.height)
+    @mixed = NArray.float(world.width, world.height)
+    @old_tree_size.fill!(0.0)
+    @decid.fill!(0.0)
+    @conifer.fill!(0.0)
+    @mixed.fill!(0.0)
+    
+    ResourceTile.where(:world_id => world.id).find_in_batches do |group|
+      group.each do |rt|
+        x = rt.x
+        y = rt.y
+        case rt.tree_species
+          when ResourceTile::Verbiage[:tree_species][:deciduous]
+            @decid[x,y] = rt.tree_size
+            @old_tree_size[x,y] = rt.tree_size
+          when ResourceTile::Verbiage[:tree_species][:coniferous]
+            @conifer[x,y] = rt.tree_size
+            @old_tree_size[x,y] = rt.tree_size
+          when ResourceTile::Verbiage[:tree_species][:mixed]
+            @mixed[x,y] = rt.tree_size
+            @old_tree_size[x,y] = rt.tree_size
+        end #case
+      end #group
+    end #find_in_batches
+
+
+    # compute the tree growth
+    result = compute_timber @decid, @conifer, @mixed, 1
+    
+    # debug code
+    #puts "Old Tree Size"
+    #print_Matrix @old_tree_size, @old_tree_size.shape[0], @old_tree_size.shape[1]
+    #puts "New Tree Size"
+    #print_Matrix result.tree_size, result.tree_size.shape[0], result.tree_size.shape[0]
+    
+    
+    # update the trees
     LandTile.where(:world_id => world.id).find_in_batches do |group|
       group.each do |rt|
-        rt.grow_trees
+        x = rt.x
+        y = rt.y
+        case rt.tree_species
+          when ResourceTile::Verbiage[:tree_species][:deciduous]
+            rt.tree_size = result.tree_size[x,y]
+          when ResourceTile::Verbiage[:tree_species][:coniferous]
+            rt.tree_size = result.tree_size[x,y]
+          when ResourceTile::Verbiage[:tree_species][:mixed]
+            rt.tree_size = result.tree_size[x,y]
+        end #case
         rt.save!
-      end
+      end #group
     end
+
   end
+
 end
