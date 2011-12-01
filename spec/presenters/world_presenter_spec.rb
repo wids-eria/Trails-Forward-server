2c5d015f7f466555dc337d489b3094132c77d000
Stephen Anderson and Nick Karpenske <dev+bendycode+randland@bendyworks.com>
2011-12-01 16:41:19 -0600

    Refactor spec

--- spec/presenters/world_presenter_spec.rb
+++ spec/presenters/world_presenter_spec.rb
@@ -2,42 +2,33 @@ require 'spec_helper'
 
 require Rails.root.join("lib/example_world_builder")
 
-describe do
-  it "test WorldPresenter to_png" do
+describe WorldPresenter do
+  let(:world) { ExampleWorldBuilder.build_example_world 6, 6 }
 
-    # create a world
-    world = ExampleWorldBuilder.build_example_world 6, 6
+  describe "#to_png" do
+    let(:canvas) { WorldPresenter.new(world) }
+    subject { canvas.to_png }
 
-    # convert the world to png
-    canvas = WorldPresenter.new(world).to_png;
-
-    # make sure the pixel value match the resource type
-    Range.new(0,world.width-1).each do |x|
-      Range.new(0,world.height-1).each do |y|
-        rt =  world.resource_tile_at x,y
-        case rt.type
-        when WaterTile.to_s
-          canvas[x,y].should == ChunkyPNG::Color::WHITE
-        when LandTile.to_s
-          canvas[x,y].should == ChunkyPNG::Color::BLACK
+    it 'colors water and land correctly' do
+      (0...world.width).each do |x|
+        (0...world.height).each do |y|
+          subject[x,y].should == case world.resource_tile_at(x,y).type
+                                 when WaterTile.to_s then ChunkyPNG::Color::WHITE
+                                 when LandTile.to_s  then ChunkyPNG::Color::BLACK
+                                 end
         end
       end
     end
 
   end
 
-  it "test WorldPresenter save_png" do
 
-    # create a world
-    world = ExampleWorldBuilder.build_example_world 6, 6
-
-    # save the world as an image
-    WorldPresenter.new(world).save_png;
-
-    # check if the image was saved
-    filename = "public/worlds/#{world.id}/images/world.png"
-    File.exists?(filename).should == true
-
+  describe "#save_png" do
+    let(:filename) { "public/worlds/#{world.id}/images/world.png" }
+    before { WorldPresenter.new(world).save_png }
+    it 'saves the map to disk' do
+      File.exists?(filename).should == true
+    end
   end
 
 end
