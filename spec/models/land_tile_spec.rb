64c40f1dd94960ba1dc1ce452186d76b4399553e
Bendyworks Pair <dev@bendyworks.com>
2011-12-02 17:43:16 -0600

    Add Land Tile spec

---
+++ spec/models/land_tile_spec.rb
@@ -0,0 +1,68 @@
+require 'spec_helper'
+
+describe LandTile do
+  it "can be clearcut if zoned for logging" do
+    tile = LandTile.new zoned_use: "Logging"
+    tile.can_be_clearcut?.should be_true
+  end
+
+  it "cant be clearcut if not zoned for logging" do
+    tile = LandTile.new zoned_use: "Park"
+    tile.can_be_clearcut?.should_not be_true
+  end
+
+  context "with world" do
+    let(:megatile) { create :megatile, owner: player }
+    let(:tile) { megatile.world.resource_tiles.select{|tile| tile.kind_of?(LandTile)}.first }
+    let(:player) { create :player }
+    before do
+      tile.megatile.owner = player
+      tile.zoned_use = "Logging"
+      tile.tree_density = 1
+      tile.tree_size = 1
+    end
+ 
+    it "clear cuts the land" do
+      lambda {
+        tile.clearcut!
+      }.should change(tile, :tree_density).to(0.0)
+    end
+
+    it "awards tile owner the lumber value when clearcut" do
+      lambda {
+        tile.clearcut!
+      }.should change { tile.megatile.owner.balance }
+    end
+  end
+
+  it "can be bulldozed" do
+    tile = LandTile.new
+    tile.can_be_bulldozed?.should be_true
+  end
+
+  it "bulldozes the land" do
+    tile = LandTile.new
+    tile.should_receive(:clear_resources)
+    tile.bulldoze!
+  end
+
+  it "has estimated value" do
+    tile = LandTile.new
+    tile.estimated_value.should > 0
+  end
+
+  context "trees" do
+    let(:tile) { LandTile.new tree_density: 1, tree_size: 1 }
+
+    it "should have estimated lumber value" do
+      tile.estimated_lumber_value.should == 42
+    end
+
+    it "can be grown" do
+      lambda {
+        tile.grow_trees
+      }.should change(tile, :tree_size)
+    end
+  end
+  
+end
