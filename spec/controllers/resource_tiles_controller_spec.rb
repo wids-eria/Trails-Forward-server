40b3a2c4c6bd00cf0561fb52cb477fb8f8cbfb95
Stephen Anderson <dev+bendycode@bendyworks.com>
2011-12-13 16:41:50 -0600

    spec for ResourceTilesController#bulldoze

--- spec/controllers/resource_tiles_controller_spec.rb
+++ spec/controllers/resource_tiles_controller_spec.rb
@@ -5,14 +5,16 @@ describe ResourceTilesController do
   include Devise::TestHelpers
   render_views
 
-  describe '#clearcut' do
-    let(:world) { create :world }
-    let(:player) { create :lumberjack, world: world }
-    let(:user) { player.user }
-    let(:megatile) { create :megatile, world: world, owner: megatile_owner }
-    let!(:water_tile) { create :water_tile, world: world, megatile: megatile }
+  let(:world) { create :world }
+  let(:player) { create :lumberjack, world: world }
+  let(:user) { player.user }
+  let(:megatile) { create :megatile, world: world, owner: megatile_owner }
+  let!(:land_tile) { create :land_tile, world: world, megatile: megatile, zoned_use: 'Logging' }
+  let!(:water_tile) { create :water_tile, world: world, megatile: megatile }
 
-    before { sign_in user }
+  before { sign_in user }
+
+  describe '#clearcut' do
 
     context 'passed an unowned tile' do
       let(:passed_tile) { water_tile }
@@ -36,12 +38,51 @@ describe ResourceTilesController do
     end
 
     context 'passed a resource tile that can be clearcut' do
-      let(:passed_tile) { create :land_tile, world: world, megatile: megatile, zoned_use: 'Logging' }
+      let(:passed_tile) { land_tile }
       let(:megatile_owner) { player }
-      subject { response }
       before { post :clearcut, :world_id => world.id, :id => passed_tile.id, format: 'json' }
 
-      it { should be_success }
+      it 'responds with success' do
+        response.should be_success
+      end
+      it 'clearcuts the resource tile' do
+        passed_tile.reload.tree_density.should == 0
+      end
+    end
+  end
+
+  describe '#bulldoze' do
+
+    context 'passed an unowned tile' do
+      let(:passed_tile) { land_tile }
+      let(:megatile_owner) { create :player }
+
+      it 'raises an access denied error' do
+        lambda{
+          post :bulldoze, :world_id => world.id, :id => passed_tile.id, format: 'json'
+        }.should raise_error(CanCan::AccessDenied, 'You are not authorized to access this page.')
+      end
+    end
+
+    context 'passed a resource tile that cannot be bulldozed' do
+      let(:passed_tile) { water_tile }
+      let(:megatile_owner) { player }
+      subject { response }
+      before { post :bulldoze, :world_id => world.id, :id => passed_tile.id, format: 'json' }
+
+      it { should be_forbidden }
+      its(:body) { should =~ /Action illegal for this land/ }
+    end
+
+    context 'passed a resource tile that can be bulldozed' do
+      let(:passed_tile) { land_tile }
+      let(:megatile_owner) { player }
+
+      it 'bulldozes the passed in tile' do
+        LandTile.any_instance.should_receive(:bulldoze!)
+        post :bulldoze, :world_id => world.id, :id => passed_tile.id, format: 'json'
+        response.should be_success
+      end
     end
   end
 end
