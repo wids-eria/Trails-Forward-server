8089bbe78204bc98723375b639ee351aa92bc541
Stephen Anderson and Nick Karpenske <dev+bendycode+randland@bendyworks.com>
2011-12-28 10:56:41 -0600

    WIP World loads in 5 min (vs 12)

---
+++ script/tribbles_everywhere.rb
@@ -0,0 +1,114 @@
+#!/usr/bin/env ./script/rails runner
+require 'benchmark'
+require "tattletail"
+
+num_ticks = ARGV[1] || 10
+num_ticks = num_ticks.to_i
+num_tribbles = 1000
+world_width = 198
+world_height = 198
+
+def file_prefix world
+  "doc/world_pngs/#{world.id}-#{world.name.gsub(/\s/, '_')}"
+end
+
+def clear_old_pngs world
+  files = Dir.glob("#{file_prefix world}*.png")
+  files.each do |file|
+    File.delete(file)
+  end
+end
+
+def create_png world, tick_count
+  file_name = "#{file_prefix world}-%03d.png" % tick_count
+  world.generate_png file_name
+end
+
+def create_gif world
+  file_name = "#{file_prefix world}.gif"
+  File.delete(file_name) if File.exists?(file_name)
+  system "convert #{file_prefix world}-*.png #{file_name}"
+end
+
+def set_progress_title tick_progress_bar, tick_count, agent_count
+  tick_progress_bar.title = "Tick #{tick_count + 1} - #{agent_count} Tribbles" if Rails.env.development?
+end
+
+def tribbles_in_world world
+  Tribble.where(world_id: world.id)
+end
+
+world = if ARGV[0]
+          World.find(ARGV[0])
+        else
+          Factory :world_with_resources, width: world_width, height: world_height
+        end
+tribbles_in_world(world).delete_all
+
+world_width = world.width
+world_height = world.height
+tick_count = 0
+ind_tick_count = 0
+tick_tiles = true
+
+ProgressBar.color_status
+ProgressBar.iter_rate_mode
+ProgressBar.disable_output unless Rails.env.development?
+
+tribble_pb = ProgressBar.new('Tribbles', num_tribbles)
+
+num_tribbles.times do
+  begin
+    tribble = Factory.build :tribble, world: world,
+                                      location: [rand * world_width, rand * world_height],
+                                      heading: rand(360).round,
+                                      age: [Agent.normal_dist(2,5), 0].max.floor
+  end until tribble.resource_tile.type == 'LandTile' && tribble.resource_tile.tree_density > 0.4
+  tribble.save
+  tribble_pb.inc
+end
+tribble_pb.finish
+
+clear_old_pngs world
+create_png world, tick_count
+
+puts
+
+tile_count = LandTile.where(world_id: world.id).where('tree_density IS NOT NULL').count
+
+num_ticks.times do |n|
+
+  tick_pb = ProgressBar.new("Tick #{tick_count + 1} - Tribbles", tribbles_in_world(world).count)
+  set_progress_title(tick_pb, tick_count, tribbles_in_world(world).count)
+  tick_pb.expand_title
+  tick_pb.colorize :magenta
+
+  tribbles_in_world(world).each do |tribble|
+    tribble.tick!
+
+    set_progress_title(tick_pb, tick_count, tribbles_in_world(world).count)
+    tick_pb.expand_title
+    tick_pb.inc
+  end
+  tick_pb.finish
+
+
+  if tick_tiles
+    tick_pb = ProgressBar.new("Tick #{tick_count + 1} - Tiles", tile_count)
+    tick_pb.expand_title
+    tick_pb.colorize :blue
+    LandTile.where(world_id: world.id).where('tree_density IS NOT NULL').find_in_batches do |tile_batch|
+      tile_batch.each do |tile|
+        tile.tick!
+        tick_pb.inc
+      end
+    end
+    tick_pb.finish
+  end
+
+  tick_count += 1
+
+  create_png world, tick_count
+  create_gif world
+  puts
+end
