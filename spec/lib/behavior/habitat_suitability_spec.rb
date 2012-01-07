c209434fb5d51c46e09db4bd92a5bb5fdd70ceea
Nick Karpenske <randland@gmail.com>
2012-01-07 17:00:03 -0600

    Fleshed out suitability specs and fixed base formula

--- spec/lib/behavior/habitat_suitability_spec.rb
+++ spec/lib/behavior/habitat_suitability_spec.rb
@@ -4,10 +4,11 @@ class HabitatSuitabilityAgent < Agent
   include Behavior::HabitatSuitability
   habitat_suitability open_water: 0,
                       dwarf_scrub: 10,
-                      shrub_scrub: 8.7
+                      shrub_scrub: 8.7,
+                      pasture_hay: 5
 
   habitat_survival_modifier do |suitability_rating|
-    (1 - (1 - (suitability_rating / 10.0) ** 3))
+    1 - (1 - suitability_rating / 10.0) ** 3
   end
 
   def go
@@ -37,13 +38,29 @@ describe HabitatSuitabilityAgent do
       agent.stub_chain(:resource_tile, land_cover_type: ResourceTile.cover_type_number(cover_code))
     end
     subject { agent.habitat_survival_modifier }
-    context 'For a 0 suitability environment' do
+
+    context 'for a non-suitable environment' do
       let(:cover_code) { :open_water }
-      it { should == 0 }
+      it { should be_kind_of(Float) }
+      it { should == 0.0 }
     end
-    context 'For a 10 suitability environment' do
+
+    context 'for an integer defined suitability environment' do
+      let(:cover_code) { :pasture_hay }
+      it { should be_kind_of(Float) }
+      it { should == 0.875 }
+    end
+
+    context 'for an float defined suitability environment' do
+      let(:cover_code) { :shrub_scrub }
+      it { should be_kind_of(Float) }
+      it { should == 0.997803 }
+    end
+
+    context 'for a perfectly suitabile environment' do
       let(:cover_code) { :dwarf_scrub }
-      it { should == 1 }
+      it { should be_kind_of(Float) }
+      it { should == 1.0 }
     end
   end
 end
