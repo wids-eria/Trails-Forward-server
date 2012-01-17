91d9addffe81c85272e3b3594c0b5edc8574627d
Kevin Alford <zeroeth@pixelflow.org>
2012-01-17 14:50:41 -0600

    Switch to mocha for stubbing

--- spec/config.rb
+++ spec/config.rb
@@ -1,7 +1,7 @@
 require 'factory_girl/syntax/methods'
 
 RSpec.configure do |config|
-  config.mock_with :rspec
+  config.mock_with :mocha
   config.treat_symbols_as_metadata_keys_with_true_values = true
   config.include FactoryGirl::Syntax::Methods
 end
