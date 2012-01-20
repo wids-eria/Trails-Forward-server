fd1d863862291b31633beabf16b8bbb031eeed2b
Nick Karpenske <dev+randland@bendyworks.com>
2012-01-20 15:35:04 -0600

    Try again

--- lib/jobs/agent_tick_job.rb
+++ lib/jobs/agent_tick_job.rb
@@ -1,5 +1,6 @@
-APP_PATH = File.expand_path('../../../config/application',  __FILE__)
 require File.expand_path('../../../config/boot',  __FILE__)
+APP_PATH = File.expand_path('../../../config/application',  __FILE__)
+require APP_PATH
 require 'stalker'
 include Stalker
 
