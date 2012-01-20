34476c99706134b10d748d3b519e45042c35435c
Nick Karpenske <dev+randland@bendyworks.com>
2012-01-20 15:44:17 -0600

    WIP Broken environment load in job

--- lib/jobs/agent_tick_job.rb
+++ lib/jobs/agent_tick_job.rb
@@ -1,6 +1,10 @@
 require File.expand_path('../../../config/boot',  __FILE__)
+
 APP_PATH = File.expand_path('../../../config/application',  __FILE__)
 require APP_PATH
+
+Rails.application.require_environment!
+
 require 'stalker'
 include Stalker
 
@@ -8,4 +12,3 @@ job 'agent_tick' do |args|
   progeny = Agent.find(args[:agent_id]).tick! || []
   Stalker.enqueue('agent_tick_complete', progeny: progeny)
 end
-
