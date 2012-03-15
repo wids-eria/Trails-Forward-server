404f197e39fdab810be080541e0ce1ee1f3e569c
Change Me <change@me.org>
2012-03-15 16:55:21 -0500

    Remove permitted actions out of megatile request

--- spec/controllers/megatiles_controller_spec.rb
+++ spec/controllers/megatiles_controller_spec.rb
@@ -25,51 +25,7 @@ describe MegatilesController do
           example 'includes bulldoze and clearcut permitted actions' do
             world.resource_tile_at(0, 0).megatile.update_attributes(owner: player)
             get :index, world_id: world.id, x_min: 0, x_max: 2, y_min: 0, y_max: 2, format: 'json'
-            json = JSON.parse(response.body)
-            megatile_hashes = json['megatiles']
-            tile_hashes = megatile_hashes.map{|megatile| megatile['resource_tiles']}.first
-            permitted_actions = tile_hashes.map{|tile| tile['permitted_actions']}
-            permitted_actions.should == Array.new(9) { ['bulldoze', 'clearcut'] }
-          end
-        end
-
-        context 'the player does not own' do
-          example 'includes no permitted actions' do
-            get :index, world_id: world.id, x_min: 0, x_max: 2, y_min: 0, y_max: 2, format: 'json'
-            json = JSON.parse(response.body)
-            megatile_hashes = json['megatiles']
-            tile_hashes = megatile_hashes.map{|megatile| megatile['resource_tiles']}.first
-            permitted_actions = tile_hashes.map{|tile| tile['permitted_actions']}
-            permitted_actions.should == Array.new(9) { [] }
-          end
-        end
-      end
-
-      context 'water tiles' do
-        before do
-          world.resource_tiles.each do |rt|
-            rt.update_attribute :type, 'WaterTile'
-          end
-        end
-        context 'the player owns' do
-          example "includes no permitted actions" do
-            world.resource_tile_at(0, 0).megatile.update_attributes(owner: player)
-            get :index, world_id: world.id, x_min: 0, x_max: 2, y_min: 0, y_max: 2, format: 'json'
-            json = JSON.parse(response.body)
-            megatile_hashes = json['megatiles']
-            tile_hashes = megatile_hashes.map{|megatile| megatile['resource_tiles']}.first
-            permitted_actions = tile_hashes.map{|tile| tile['permitted_actions']}
-            permitted_actions.should == Array.new(9) { [] }
-          end
-        end
-        context 'the player does not own' do
-          example "includes no permitted actions" do
-            get :index, world_id: world.id, x_min: 0, x_max: 2, y_min: 0, y_max: 2, format: 'json'
-            json = JSON.parse(response.body)
-            megatile_hashes = json['megatiles']
-            tile_hashes = megatile_hashes.map{|megatile| megatile['resource_tiles']}.first
-            permitted_actions = tile_hashes.map{|tile| tile['permitted_actions']}
-            permitted_actions.should == Array.new(9) { [] }
+            response.should be_success
           end
         end
       end
