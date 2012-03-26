8e17434159b9b0d93dca1b808a21464667c3e44b
Kevin Alford <zeroeth@pixelflow.org>
2012-03-26 11:53:48 -0500

    Fix megatile grouping factory failing in original uses

--- spec/factories/megatile_grouping_factory.rb
+++ spec/factories/megatile_grouping_factory.rb
@@ -6,7 +6,12 @@ FactoryGirl.define do
     end
 
     megatiles do
-      new_megatiles = single_megatile ? [Factory.create(:megatile, owner: owner, world: owner.world)] : []
+      # these factories are too interdependent, causing stray worlds.
+      if owner.present?
+        new_megatiles = single_megatile ? [Factory.create(:megatile, owner: owner, world: owner.world)] : []
+      else
+        new_megatiles = single_megatile ? [Factory.create(:megatile, owner: owner)] : []
+      end
     end
 
   end
