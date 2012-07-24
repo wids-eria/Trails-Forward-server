310088151fba18ecd1313790af9d69630467207c
Kevin Alford <zeroeth@pixelflow.org>
2012-07-23 23:24:40 -0500

    Quiet staging log

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -20,7 +20,7 @@ TrailsForwardWorld::Application.configure do
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
 
-  config.log_level = :debug
+  config.log_level = :info
 
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
