fa35d1e7790c40bac6e108a369d470a8b65067d9
Kevin Alford <zeroeth@pixelflow.org>
2012-09-20 15:00:37 -0500

    water tiles are land tiles are water tiles are land tiles

--- app/models/water_tile.rb
+++ app/models/water_tile.rb
@@ -3,16 +3,47 @@ class WaterTile < ResourceTile
   validate :appropriate_zoning
 
   # there doesn't seem to be a way to have extension follow inheritance
-  api_accessible :resource do |template|
-    template.add :id
-    template.add :x
-    template.add :y
+  #api_accessible :resource do |template|
+  #  template.add :id
+  #  template.add :x
+  #  template.add :y
+  #  template.add :tree_density
+  #  template.add :land_cover_type
+  #  template.add :tree_size
+  #  template.add :type
+  #end
+
+  api_accessible :resource, :extend => :resource_base do |template|
+    template.add :primary_use
+    template.add :zoning_code
+    template.add :people_density
+    template.add :housing_density
     template.add :tree_density
     template.add :land_cover_type
     template.add :tree_size
-    template.add :type
+    template.add :development_intensity
+    template.add :imperviousness
+    template.add :num_2_inch_diameter_trees
+    template.add :num_4_inch_diameter_trees
+    template.add :num_6_inch_diameter_trees
+    template.add :num_8_inch_diameter_trees
+    template.add :num_10_inch_diameter_trees
+    template.add :num_12_inch_diameter_trees
+    template.add :num_14_inch_diameter_trees
+    template.add :num_16_inch_diameter_trees
+    template.add :num_18_inch_diameter_trees
+    template.add :num_20_inch_diameter_trees
+    template.add :num_22_inch_diameter_trees
+    template.add :num_24_inch_diameter_trees
+    template.add :housing_capacity
+    template.add :housing_occupants
+    template.add :harvest_area
+    template.add :supported_saplings
+    template.add :tree_type
+    template.add :outpost
+    template.add :marten_population
+    template.add :vole_population
   end
-
   def tick
 
   end
