eec3b08d24582ad502c47d706018a4843c0509c8
Kevin Alford <zeroeth@pixelflow.org>
2012-09-25 19:35:29 -0500

    Creating messages!

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,4 +3,5 @@ class Message < ActiveRecord::Base
   belongs_to :sender,    class_name: 'Player'
 
   validates :subject, :body, :sender_id, :recipient_id, presence: true
+  attr_accessible :subject, :body, :recipient_id
 end
--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -29,45 +29,32 @@ describe MessagesController do
     message = build :message
     message.attributes
   end
-  
-  # This should return the minimal set of values that should be in the session
-  # in order to pass any filters (e.g. authentication) defined in
-  # MessagesController. Be sure to keep this updated too.
-  def valid_session
-    {}
-  end
 
-  let!(:message) { create :message }
+
+  let!(:message)  { create(:message, sender: player2, recipient: logged_in_player) }
+  let!(:message_to_other_player)  { create(:message, sender: logged_in_player, recipient: player2) }
+  let!(:logged_in_player) { create :player }
+  let!(:player2) { create :player }
+  let(:world) { message.recipient.world }
+  let(:shared_params) { {world_id: world.to_param, format: 'json'} }
 
   before do
-    sign_in message.recipient.user
+    sign_in logged_in_player.user
   end
 
   describe "GET index" do
     it "assigns all messages as @messages" do
-      get :index, world_id: message.recipient.world, format: 'json'
-      assigns(:messages).should eq([message])
+      get :index, shared_params
+      response.should be_successful
+      response.body.should have_content(message.body)
     end
   end
 
   describe "GET show" do
     it "assigns the requested message as @message" do
-      get :show, {:id => message.to_param}, valid_session
-      assigns(:message).should eq(message)
-    end
-  end
-
-  describe "GET new" do
-    it "assigns a new message as @message" do
-      get :new, {}, valid_session
-      assigns(:message).should be_a_new(Message)
-    end
-  end
-
-  describe "GET edit" do
-    it "assigns the requested message as @message" do
-      get :edit, {:id => message.to_param}, valid_session
-      assigns(:message).should eq(message)
+      get :show, shared_params.merge(id: message.to_param)
+      response.should be_successful
+      response.body.should have_content(message.body)
     end
   end
 
@@ -75,89 +62,16 @@ describe MessagesController do
     describe "with valid params" do
       it "creates a new Message" do
         expect {
-          post :create, {:message => valid_attributes}, valid_session
+          post :create, shared_params.merge(:message => valid_attributes.merge(:recipient_id => player2.id))
         }.to change(Message, :count).by(1)
-      end
 
-      it "assigns a newly created message as @message" do
-        post :create, {:message => valid_attributes}, valid_session
-        assigns(:message).should be_a(Message)
-        assigns(:message).should be_persisted
-      end
-
-      it "redirects to the created message" do
-        post :create, {:message => valid_attributes}, valid_session
-        response.should redirect_to(Message.last)
-      end
-    end
-
-    describe "with invalid params" do
-      it "assigns a newly created but unsaved message as @message" do
-        # Trigger the behavior that occurs when invalid params are submitted
-        Message.any_instance.stub(:save).and_return(false)
-        post :create, {:message => {}}, valid_session
-        assigns(:message).should be_a_new(Message)
-      end
-
-      it "re-renders the 'new' template" do
-        # Trigger the behavior that occurs when invalid params are submitted
-        Message.any_instance.stub(:save).and_return(false)
-        post :create, {:message => {}}, valid_session
-        response.should render_template("new")
+        message = Message.last
+        message.sender.should == logged_in_player
+        message.recipient.should == player2
       end
     end
   end
 
-  describe "PUT update" do
-    describe "with valid params" do
-      it "updates the requested message" do
-        # Assuming there are no other messages in the database, this
-        # specifies that the Message created on the previous line
-        # receives the :update_attributes message with whatever params are
-        # submitted in the request.
-        Message.any_instance.should_receive(:update_attributes).with({'these' => 'params'})
-        put :update, {:id => message.to_param, :message => {'these' => 'params'}}, valid_session
-      end
-
-      it "assigns the requested message as @message" do
-        put :update, {:id => message.to_param, :message => valid_attributes}, valid_session
-        assigns(:message).should eq(message)
-      end
-
-      it "redirects to the message" do
-        put :update, {:id => message.to_param, :message => valid_attributes}, valid_session
-        response.should redirect_to(message)
-      end
-    end
-
-    describe "with invalid params" do
-      it "assigns the message as @message" do
-        # Trigger the behavior that occurs when invalid params are submitted
-        Message.any_instance.stub(:save).and_return(false)
-        put :update, {:id => message.to_param, :message => {}}, valid_session
-        assigns(:message).should eq(message)
-      end
-
-      it "re-renders the 'edit' template" do
-        # Trigger the behavior that occurs when invalid params are submitted
-        Message.any_instance.stub(:save).and_return(false)
-        put :update, {:id => message.to_param, :message => {}}, valid_session
-        response.should render_template("edit")
-      end
-    end
-  end
-
-  describe "DELETE destroy" do
-    it "destroys the requested message" do
-      expect {
-        delete :destroy, {:id => message.to_param}, valid_session
-      }.to change(Message, :count).by(-1)
-    end
-
-    it "redirects to the messages list" do
-      delete :destroy, {:id => message.to_param}, valid_session
-      response.should redirect_to(messages_url)
-    end
-  end
-
+  it 'can be marked as read'
+  it 'can be marked as archived'
 end
