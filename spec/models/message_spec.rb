41fd125e3e7c9e4d0177febd3c311e69735257bd
Kevin Alford <zeroeth@pixelflow.org>
2012-09-28 13:15:57 -0500

    Message test fix

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,7 +1,9 @@
 require 'spec_helper'
 
 describe Message do
-  let(:message) { create :message }
+  let(:sender) { create :player }
+  let(:recipient) { create :player }
+  let(:message) { create :message, sender: sender, recipient: recipient }
   it 'has a sender and recipient' do
     message.sender.should be_a Player
     message.recipient.should be_a Player
