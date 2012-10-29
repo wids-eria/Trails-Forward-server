3a4d804bcac1f04f641689aa9e5c357ae4a08cac
Ben Shapiro <ben@getdown.org>
2012-10-29 16:47:00 -0500

    typo fixed

--- app/controllers/non_player_characters_controller.rb
+++ app/controllers/non_player_characters_controller.rb
@@ -18,7 +18,7 @@ class NonPlayerCharactersController < ApplicationController
   def create
     if non_player_character.save
       flash[:notice] = "Character Created"
-      respond_with non_player_character, notice: 'Created!', location: world_non_player_character_path
+      respond_with non_player_character, notice: 'Created!', location: world_non_player_characters_path(world.id)
     else
       render :new
     end
@@ -27,6 +27,6 @@ class NonPlayerCharactersController < ApplicationController
   def update
     flash[:notice] = "Character Updated" if non_player_character.update_attributes (params[:non_player_character]||params[:company])
 
-    respond_with non_player_character, location: world_non_player_character_path
+    respond_with non_player_character, location: world_non_player_characters_path(world.id)
   end
 end
